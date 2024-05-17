3b0f528913488c37990bacd83d0ef7ff3e6e5d8f
chris <chris@amiable.dev>
2024-05-17 11:13:54 +0100

    Updated header and footer links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,7 +80,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/amiable-dev/amiable-docusaurus',
             label: 'GitHub',
             position: 'right',
           },
@@ -99,20 +99,8 @@ const config = {
             ],
           },
           {
-            title: 'Community',
+            title: 'Social',
             items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
             ],
           },
           {
@@ -124,7 +112,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/amiable-dev',
               },
             ],
           },
