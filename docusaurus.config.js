46ad6edc2ea4eab216a0f1c2ef47fafd7afbc9ea
chris <chris@amiable.dev>
2024-05-20 10:36:30 +0100

    Updated edit links for Blog and Docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/amiable-dev/amiable-docusaurus/edit/main/docs/',
+            'https://github.com/amiable-dev/amiable-docusaurus/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/amiable-dev/amiable-docusaurus/edit/main/blog/',
+            'https://github.com/amiable-dev/amiable-docusaurus/edit/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
