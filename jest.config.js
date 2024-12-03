6eab777e276bb32a2e13dc9364a841c5ccf5815a
Chris <chris@amiable.dev>
2024-12-03 07:34:42 +0000

    Fix babel preset path and jest configuration

--- jest.config.js
+++ jest.config.js
@@ -4,13 +4,12 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['/node_modules/', '/.docusaurus/', '/build/'],
   transform: {
-    '^.+\\.[jt]sx?$': ['@docusaurus/core/lib/babel/jest.transform.js'],
+    '^.+\\.[jt]sx?$': ['babel-jest', { presets: ['@docusaurus/core/lib/babel/preset'] }],
   },
   moduleNameMapper: {
     '^@/components/ui/(.*)$': '<rootDir>/src/components/ui/$1',
     '^@theme-original/(.*)$': '<rootDir>/node_modules/@docusaurus/theme-classic/lib/theme/$1',
     '^@site/(.*)$': '<rootDir>/$1',
-    '^@docusaurus/(.*)': '<rootDir>/node_modules/@docusaurus/core/lib/$1',
   },
   collectCoverageFrom: [
     'src/theme/**/*.{js,jsx,ts,tsx}',
