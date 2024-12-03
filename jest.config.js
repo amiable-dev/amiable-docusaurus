c4871f555fe4eb5b6f38d43bcca0cf585324723b
Chris <chris@amiable.dev>
2024-12-03 07:32:12 +0000

    Fix Jest configuration for Docusaurus babel transform

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,13 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['/node_modules/', '/.docusaurus/', '/build/'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['@docusaurus/core/babel'] }],
+    '^.+\\.[jt]sx?$': ['@docusaurus/core/lib/babel/jest.transform.js'],
   },
   moduleNameMapper: {
     '^@/components/ui/(.*)$': '<rootDir>/src/components/ui/$1',
     '^@theme-original/(.*)$': '<rootDir>/node_modules/@docusaurus/theme-classic/lib/theme/$1',
     '^@site/(.*)$': '<rootDir>/$1',
+    '^@docusaurus/(.*)': '<rootDir>/node_modules/@docusaurus/core/lib/$1',
   },
   collectCoverageFrom: [
     'src/theme/**/*.{js,jsx,ts,tsx}',
