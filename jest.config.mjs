464a713c0c3e729536ecd226efd436d1ea7deb64
Chris <chris@amiable.dev>
2024-12-03 07:44:29 +0000

    Update Jest configuration to match Docusaurus standards

---
+++ jest.config.mjs
@@ -0,0 +1,39 @@
+import {fileURLToPath} from 'url';
+
+const config = {
+  rootDir: fileURLToPath(new URL('.', import.meta.url)),
+  verbose: true,
+  testURL: 'http://localhost/',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '__fixtures__',
+    'build',
+    '.docusaurus',
+  ],
+  transform: {
+    '^.+\\.[jt]sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          target: 'es2020',
+        },
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^@theme/(.*)$': '@docusaurus/theme-classic/lib/theme/$1',
+    '^@docusaurus/(.*)$': '@docusaurus/$1',
+    '^@site/(.*)$': '<rootDir>/$1',
+  },
+  transformIgnorePatterns: ['/node_modules/(?!(eta|@docusaurus)/)'],
+  fakeTimers: {
+    enableGlobally: true,
+  },
+  snapshotSerializers: [require.resolve('snapshot-serializer-beautifier')],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  coveragePathIgnorePatterns: ['/node_modules/', '__tests__'],
+};
+
+export default config;
